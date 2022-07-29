ce830676cf43370678d4d5f6d7a4351e4e440f0e
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-29 13:11:36 +0100

    update

--- user_validator.php
+++ user_validator.php
@@ -10,6 +10,30 @@ class UserValidator {
     {
          $this -> data = $userdata;
     }
+
+    public function ValidateForm()
+    {
+        foreach (self::$fields as $field ) {
+            if (array_key_exist($field, $this -> data)) {
+                # code...
+            }
+        }
+    }
+
+    private function ValidateUserName()
+    {
+        # code...
+    }
+
+    private function ValidateUserEmail()
+    {
+        # code...
+    }
+
+    private function AddError()
+    {
+        # code...
+    }
 }
 
 ?>
