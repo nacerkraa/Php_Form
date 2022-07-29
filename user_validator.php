8b3be2c4b6de04be6ead57d56f53df7ff1ce7d16
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-29 12:57:48 +0100

    update

--- user_validator.php
+++ user_validator.php
@@ -1,9 +1,14 @@
 <?php
 
 class UserValidator {
+
+    private $data;
+    private $errors = [];
+    public static $fields= ['username', 'email'];
+
     public function __construct()
     {
-         
+         $this -> data = $userdata;
     }
 }
 
